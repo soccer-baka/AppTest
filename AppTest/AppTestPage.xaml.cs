88061dd0234284f4dcdaf79b33f50f7939ddae94
soccer-baka <Hideya@sugiyama-no-iMac.local>
2017-03-02 19:31:55 -0500

    1st version for AppTest

---
+++ AppTest/AppTestPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace AppTest
+{
+    class ListItem
+    {
+        public string Name { get; set; }
+        public string Comment { get; set; }
+        public ListItem(string name, string comment)
+        {
+            Name = name;
+            Comment = comment;
+        }
+    }
+
+    public partial class AppTestPage : ContentPage
+    {
+        public AppTestPage()
+        {
+            InitializeComponent();
+
+            ListItem[] items = { new ListItem("test 1", "comment 1"), new ListItem("test 2", "comment 2"), new ListItem("test 3", "comment 3") };
+            listView.ItemsSource = items;
+        }
+    }
+}
